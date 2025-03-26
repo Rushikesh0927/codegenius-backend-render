f4f74dcfa519288c8c287ed275e0291227fd89f0
Yemul <yemul@Rushi.com>
2025-03-26 22:22:59 +0530

    Fix backend API with server.js and improved testing tools

---
+++ server.js
@@ -0,0 +1,7 @@
+// This file serves as the entry point for Render deployment
+// It simply requires the main index.js file to run the server
+
+console.log('Starting CodeGenius Backend Server...');
+
+// Load the main application file
+require('./index.js'); 
